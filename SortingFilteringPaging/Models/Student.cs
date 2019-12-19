8e8ba7085e8f5247bd4b83bf6a79fde8de7d7374
Moemen Badawi <moemenbadawi@hotmail.com>
2019-12-19 08:58:08 +0100

    Add project files.

---
+++ SortingFilteringPaging/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SortingFilteringPaging.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public List<Enrollment> Enrollments { get; set; }
+    }
+}
